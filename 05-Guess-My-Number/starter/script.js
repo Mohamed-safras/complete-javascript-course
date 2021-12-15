fc3811fbe96b5046e105b5de06588e544347d610
Mohamed Safras <awsafras@gmail.com>
2021-12-15 11:17:26 +0530

    finished

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,18 +2,52 @@
 
 const number = document.querySelector('.number');
 const guessContent = document.querySelector('.guess');
-const highScore = document.querySelector('.highscore');
+const highScoreContent = document.querySelector('.highscore');
 const scoreContent = document.querySelector('.score');
 const check = document.querySelector('.check');
 const again = document.querySelector('.again');
 const message = document.querySelector('.message');
 
 let score = 20;
+let highScore = 0;
 let randomNumber = Math.floor(Math.random() * 20) + 1;
-
 check.addEventListener('click', function () {
-  number.textContent = randomNumber;
-  const guessValue = guessContent.value;
+  const guessValue = Number(guessContent.value);
 
-  console.log(guessValue);
+  if (!guessValue) {
+    message.textContent = 'No Number';
+  } else if (guessValue !== randomNumber) {
+    if (score < 1) {
+      message.textContent = 'You lost the game';
+      scoreContent.textContent = 0;
+    } else {
+      message.textContent = `Guess value is too ${
+        guessValue > randomNumber ? 'high' : 'low'
+      } `;
+      score--;
+      scoreContent.textContent = score;
+    }
+  } else if (guessValue === randomNumber) {
+    number.textContent = randomNumber;
+    message.textContent = 'You won the game';
+    document.body.style.backgroundColor = '#60b347';
+    guessContent.style.display = 'none';
+    check.style.display = 'none';
+    if (score > highScore) {
+      highScore = score;
+      highScoreContent.textContent = highScore;
+    }
+  }
+});
+
+again.addEventListener('click', function () {
+  guessContent.style.display = 'block';
+  check.style.display = 'block';
+  guessContent.value = '';
+  document.body.style.background = '#222';
+  message.textContent = 'Start guessing...';
+  score = 20;
+  scoreContent.textContent = score;
+  number.textContent = '?';
+  randomNumber = Math.floor(Math.random() * 20) + 1;
 });
